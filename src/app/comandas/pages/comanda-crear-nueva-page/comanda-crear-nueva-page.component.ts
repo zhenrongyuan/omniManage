7317aa5e8bdb2a38b83664a916330a4d956f5770
Zhenrong Yuan Mac <zhenrong.yuanlu@gmail.com>
2023-05-13 19:33:39 -0400

    ImprimirComanda()
    Incorpora función para imprimir comanda.

--- src/app/comandas/pages/comanda-crear-nueva-page/comanda-crear-nueva-page.component.ts
+++ src/app/comandas/pages/comanda-crear-nueva-page/comanda-crear-nueva-page.component.ts
@@ -4,7 +4,9 @@ import { Tipo } from 'src/app/shared/interfaces/Tipo';
 import { TipoProductoService } from 'src/app/shared/services/tipoProducto.service';
 import { Producto } from 'src/app/productos/interfaces/producto';
 import { ProductoService } from 'src/app/productos/services/producto.service';
-import { comandaNueva } from '../../interfaces/comandaNueva';
+import { comandaNueva, cuenta, cuenta as cuentaNueva } from '../../interfaces/comandaNueva';
+import { Cuenta } from 'src/app/shared/interfaces/Cuenta';
+import * as printJS from 'print-js';
 
 @Component({
   selector: 'app-comanda-crear-nueva-page',
@@ -29,7 +31,6 @@ export class ComandaCrearNuevaPageComponent implements OnInit{
   }
 
   filtrar(clave: string){
-    console.log(clave);
     
     this.productoService.getProductos()
     .subscribe( producto => 
@@ -39,7 +40,47 @@ export class ComandaCrearNuevaPageComponent implements OnInit{
 
   actualizarDetalles(detalles: Detalle[]):void{
     this.comandaNueva.detalle = detalles;
-   
-    
+    console.log(this.comandaNueva.detalle);
+
+  }
+
+  onCloseModal():void{
+    this.reinicializarValores();
+    this.comandaNueva.cuenta = cuentaNueva;
+    const cuenta: Cuenta = this.calcularComanda(comandaNueva);
+    this.comandaNueva.cuenta = cuenta;
+  }
+
+  calcularComanda(comanda: Comanda):Cuenta {
+
+    const cuenta = cuentaNueva;
+
+    comanda.detalle.forEach( detalle => {
+      cuenta.subtotal += detalle.monto_neto;
+      
+    });
+
+    cuenta.propina = cuenta.subtotal * 0.1;
+    cuenta.total = cuenta.subtotal + cuenta.propina;
+
+    return cuenta;
+
+  }
+
+  reinicializarValores():void{
+    cuentaNueva.subtotal = 0;
+    cuentaNueva.propina = 0;
+    cuentaNueva.total = 0;
+    cuentaNueva.comentario = '';
+  }
+
+  imprimirComanda(comanda: Comanda){
+    printJS({
+      printable: comanda._id,
+      type: 'html',
+      targetStyle: ['*'],
+      css: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css',
+      style: '.card{ padding: 0, margin: 0, font-family: Arial} '
+    });
   }
 }
