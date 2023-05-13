05a479d26a2548ddc0f2817ed525caccfaaf010b
Zhenrong Yuan Mac <zhenrong.yuanlu@gmail.com>
2023-05-13 13:44:17 -0400

    TipoProductoService
    Incorpora TipoProductoService

--- src/app/productos/pages/productos-page/productos-page.component.ts
+++ src/app/productos/pages/productos-page/productos-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Producto } from '../../interfaces/producto';
 import { ProductoService } from '../../services/producto.service';
+import { TipoProductoService } from 'src/app/shared/services/tipoProducto.service';
+import { Tipo } from 'src/app/shared/interfaces/Tipo';
 
 @Component({
   selector: 'productos-page',
@@ -10,14 +12,31 @@ import { ProductoService } from '../../services/producto.service';
 export class ProductosPageComponent implements OnInit{
 
   public productos: Producto[] = [];
+  public tiposProducto: Tipo[] = [];
 
-  constructor( private productoService: ProductoService) {}
+  constructor( private productoService: ProductoService,
+               private tipoProductoService: TipoProductoService, 
+    ) {}
 
   ngOnInit(): void {
+
     this.productoService.getProductos()
       .subscribe( producto => 
         this.productos = producto['productos']
         );
+
+    this.tipoProductoService.getTiposProducto()
+      .subscribe( tiposProducto =>
+        this.tiposProducto = tiposProducto['tiposProducto']
+      );    
+  }
+
+  filtrar(clave: string){
+
+    this.productoService.getProductos()
+    .subscribe( producto => 
+      this.productos = producto['productos'].filter( producto => producto.tipo.clave === clave )
+      );
   }
 
 }
